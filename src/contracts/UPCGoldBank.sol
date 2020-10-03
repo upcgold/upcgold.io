34f16b9568161c07a1fedf63096e66786bb6ded9
cad <cad@srvr.com>
2020-10-03 18:51:07 +0000

    adding a address -> lease mapping

--- src/contracts/UPCGoldBank.sol
+++ src/contracts/UPCGoldBank.sol
@@ -20,8 +20,26 @@ contract UPCGoldBank {
         bool     isOwned;
         uint     interestGained;
         uint     stakingStartTimestamp;
+        bytes32  upcHash;
     }
+
     
+    //declare an array of AddressToLeases
+    //each will have the address of the staker and the upcHash/meta
+    
+    //on deposit, adddress the following element:
+    // add the staker to the AddressToLease
+    // AddressToLease a2l;
+    // a2l.lm.staker = staker;
+    //addressToLease[staker].push();
+    //addressToLease[staker].leaseMeta[upcHash].staker = staker;
+    //addressToLease[staker].leaseMeta[upcHash].staker = staker;
+    //addressToLease[staker].leaseMeta[upcHash].staker = staker;
+
+    
+    
+    
+    mapping(address  => LeaseMeta[])   public addressToLease;       //pass in the upcId and look up the meta about the upc
     mapping(address => Balance)     public balanceReceived;
     mapping(bytes32  => LeaseMeta)   public scannables;       //pass in the upcId and look up the meta about the upc
     uint public actionPot;
@@ -39,7 +57,11 @@ contract UPCGoldBank {
     function getAddressBalance() public view returns(uint) {
         return balanceReceived[msg.sender].totalBalance;
     }
-
+    
+    function getScannableCount() public view returns(uint) {
+        return addressToLease[msg.sender].length;
+    }
+    
     function getCostToEvict(string memory upcId) public view returns(address currentStaker, uint currentAmountStaked, bool currentIsOwned, bytes32 upcHash) {
         upcHash = sha256(abi.encodePacked(upcId));
         currentStaker = scannables[upcHash].staker;
@@ -65,33 +87,53 @@ contract UPCGoldBank {
         
         if(msg.sender != scannables[upcHash].staker) {
             uint evictionPrice = evict(upcHash);
+            
             //after an eviction, currentAmountStaked should equal zero to make way for the new owner's currentAmountStaked
             currentAmountStaked = scannables[upcHash].amountStaked;
         }
+
 
         LeaseMeta memory lm;
         lm.staker = msg.sender;
         lm.amountStaked = currentAmountStaked + _addToBalance;
         lm.isOwned =  false;
         lm.stakingStartTimestamp = now;
+        lm.upcHash = upcHash;
         scannables[upcHash] = lm;
         
+        
+        //look through the current scannables and only add if 
+        //the upcId is not already with user.  dont want to add new entry if user increases their stake
+        bool addScannableToAddress = true;
+
+        
+        
+        for(uint i = 0; i< addressToLease[msg.sender].length; i++) {
+            if(addressToLease[msg.sender][i].upcHash == upcHash) {
+                addScannableToAddress = false;
+            }
+        }
+        
+        if(addScannableToAddress) {
+            addressToLease[msg.sender].push(lm);
+        }
+
         //require(balanceReceived[msg.sender].totalBalance >= _amount, "Insufficient funds for internal transfer" );
 
         balanceReceived[msg.sender].totalBalance += _addToBalance;
         Deposit memory deposit = Deposit(_addToBalance, now);
         balanceReceived[msg.sender].deposits[balanceReceived[msg.sender].numPayments] = deposit;
         balanceReceived[msg.sender].numPayments++;
-        
+
         address payable _actionPot = address(0x22F23F59A19a5EEd1eE9c546F64CC645B92a4263);
         _actionPot.transfer(_addToActionPot);
         actionPot += _addToActionPot;
-        
+
     }
     
     function calculateFee(uint amount) external pure returns (uint) {
         require((amount / 10000) * 10000 == amount , 'too small');
-        return amount * 185 / 10000;
+        return amount * 200 / 10000;  //2%
     }
 
     function evict(bytes32 upcHash) private returns (uint) {
@@ -106,13 +148,22 @@ contract UPCGoldBank {
     
     function withdraw(string memory upcId) public {
         bytes32 upcHash = sha256(abi.encodePacked(upcId));
-        require(msg.sender == scannables[upcHash].staker, "Only the staker can withdraw" );
-        uint toWithdraw = scannables[upcHash].amountStaked;
+        require(msg.sender == scannables[upcHash].staker, "Only the staker can withdraw funds" );
+        
+        uint _addToActionPot = this.calculateFee(scannables[upcHash].amountStaked);
+        uint toWithdraw = scannables[upcHash].amountStaked - _addToActionPot;
+
         address payable _to = msg.sender;
-        balanceReceived[msg.sender].totalBalance -= toWithdraw;
-        scannables[upcHash].amountStaked -= toWithdraw;
+        balanceReceived[msg.sender].totalBalance = 0;
+        scannables[upcHash].amountStaked = 0;
         scannables[upcHash].staker = address(0x0);
-        _to.transfer(toWithdraw - 1);
+
+        address payable _actionPot = address(0x22F23F59A19a5EEd1eE9c546F64CC645B92a4263);
+        _actionPot.transfer(_addToActionPot);
+        actionPot += _addToActionPot;
+
+        _to.transfer(toWithdraw);
     }
     
 }
+
