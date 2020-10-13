8f327314c0bfc6799fadc492b75996358c8f8e54
Your Name <you@example.com>
2020-10-13 14:11:25 -0700

    added a test card to the leases tab

--- src/components/Leases.js
+++ src/components/Leases.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import dai from '../dai.png'
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 class Main extends Component {
 
@@ -12,30 +15,70 @@ class Main extends Component {
 
 
     this.getScannables = this.getScannables.bind(this);
+    this.buildCard = this.buildCard.bind(this);
+  }
+
+
+
+  componentDidMount = async () => {
+    var sc = this.getScannables();
+    var self = this;
+    sc.then(function(result){
+	  console.log(result.length);
+	  result = String(result);
+	  var scannable;
+          for(var i=0; i< result.length; i++)
+          {
+	    scannable = self.buildCard(result.substring(0,10));
+          }
+	  self.setState({scannables:scannable});
+    });
   }
 
   getScannables = async () => {
     const { accounts, contract } = this.state;
-
-
     let scannables = await this.props.getMyScannables();
     return scannables;
-
   };
 
+  buildCard = (data) => {
+     return (
+      <Card>
+       <Card.Header>
+         <Nav variant="tabs" defaultActiveKey="#first">
+           <Nav.Item>
+             <Nav.Link href="#first">Active</Nav.Link>
+           </Nav.Item>
+           <Nav.Item>
+             <Nav.Link href="#link">Link</Nav.Link>
+           </Nav.Item>
+           <Nav.Item>
+             <Nav.Link href="#disabled" disabled>
+               Disabled
+             </Nav.Link>
+           </Nav.Item>
+         </Nav>
+       </Card.Header>
+       <Card.Body>
+         <Card.Title>{data}</Card.Title>
+         <Card.Text>
+           With supporting text below as a natural lead-in to additional content.
+         </Card.Text>
+         <Button variant="primary">Go somewhere</Button>
+       </Card.Body>
+     </Card>);
+  }
 
   render() {
-    var sc = this.getScannables();
-    var self = this;
-    sc.then(function(result){
-	  self.setState({scannables:result});
-    });
     return (
       <div id="content" className="mt-3">
 
         <div className="card mb-4" >
 
           <div className="card-body">
+
+
+
 
             <form className="mb-3" onSubmit={(event) => {
                 event.preventDefault()
