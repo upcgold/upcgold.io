d292cd6da781de43e3945e68b0abc606da516197
cad <cad@srvr.com>
2020-12-08 10:13:51 +0000

    Updates

---
+++ precache-manifest.b6c4e3c531852844698a869c17b28354.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b74750c53ef15d271bd7",
+    "url": "/upcgold.io/static/css/main.25da20d4.chunk.css"
+  },
+  {
+    "revision": "b74750c53ef15d271bd7",
+    "url": "/upcgold.io/static/js/main.b74750c5.chunk.js"
+  },
+  {
+    "revision": "f20cb2babbc53bb5d3a5",
+    "url": "/upcgold.io/static/css/1.967078c4.chunk.css"
+  },
+  {
+    "revision": "f20cb2babbc53bb5d3a5",
+    "url": "/upcgold.io/static/js/1.f20cb2ba.chunk.js"
+  },
+  {
+    "revision": "81eebe44cc1dc6203f86",
+    "url": "/upcgold.io/static/js/runtime~main.81eebe44.js"
+  },
+  {
+    "revision": "446a752b7d513f396a7a91a895a7425f",
+    "url": "/upcgold.io/static/media/infinity-loader2.446a752b.gif"
+  },
+  {
+    "revision": "034b04720d2817c41792aa65210389df",
+    "url": "/upcgold.io/index.html"
+  }
+];
