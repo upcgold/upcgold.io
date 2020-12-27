abd43136327ef328a498d36a13aa61aca01c189a
Your Name <you@example.com>
2020-12-26 22:21:39 -0800

    Updates

---
+++ precache-manifest.9309285decb33dc5e34ce28f466b0658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "989283b7bea95c0a199f",
+    "url": "/upcgold.io/static/css/main.52f54d9b.chunk.css"
+  },
+  {
+    "revision": "989283b7bea95c0a199f",
+    "url": "/upcgold.io/static/js/main.989283b7.chunk.js"
+  },
+  {
+    "revision": "70cf5d7539e7a327a989",
+    "url": "/upcgold.io/static/css/1.5f450f29.chunk.css"
+  },
+  {
+    "revision": "70cf5d7539e7a327a989",
+    "url": "/upcgold.io/static/js/1.70cf5d75.chunk.js"
+  },
+  {
+    "revision": "81eebe44cc1dc6203f86",
+    "url": "/upcgold.io/static/js/runtime~main.81eebe44.js"
+  },
+  {
+    "revision": "446a752b7d513f396a7a91a895a7425f",
+    "url": "/upcgold.io/static/media/infinity-loader2.446a752b.gif"
+  },
+  {
+    "revision": "a0e5c764ce94fe9041fbe85036c3e071",
+    "url": "/upcgold.io/index.html"
+  }
+];
