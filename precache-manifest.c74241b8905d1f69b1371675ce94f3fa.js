67e8dd5363dc8edbe24a9669d0843c223b94be31
Your Name <you@example.com>
2020-12-30 04:21:55 -0800

    Updates

---
+++ precache-manifest.c74241b8905d1f69b1371675ce94f3fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b62026dc924c4f3dfca8",
+    "url": "/upcgold.io/static/css/main.76ba126e.chunk.css"
+  },
+  {
+    "revision": "b62026dc924c4f3dfca8",
+    "url": "/upcgold.io/static/js/main.b62026dc.chunk.js"
+  },
+  {
+    "revision": "f192191b002f8749039a",
+    "url": "/upcgold.io/static/css/1.b6e33804.chunk.css"
+  },
+  {
+    "revision": "f192191b002f8749039a",
+    "url": "/upcgold.io/static/js/1.f192191b.chunk.js"
+  },
+  {
+    "revision": "81eebe44cc1dc6203f86",
+    "url": "/upcgold.io/static/js/runtime~main.81eebe44.js"
+  },
+  {
+    "revision": "446a752b7d513f396a7a91a895a7425f",
+    "url": "/upcgold.io/static/media/infinity-loader2.446a752b.gif"
+  },
+  {
+    "revision": "9c3e4decf95133ccf1c3cebd5bae0dfb",
+    "url": "/upcgold.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/upcgold.io/precache-manifest.0ca5fb246959d943728a4c850c7a89d2.js"
+  "/upcgold.io/precache-manifest.c74241b8905d1f69b1371675ce94f3fa.js"
 );
 
 workbox.clientsClaim();
