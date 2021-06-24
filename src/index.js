7717f66bd9a5b0976afcc9963b67eaa3d322091d
anon@anon.anon <anon@anon.anon>
2021-06-24 04:42:07 -0700

    simple hashrouting working

--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import Intel from './components/Intel';
+import { HashRouter, Route, Link } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const routing = (
+  <HashRouter>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/intel">Intel</Link>
+        </li>
+      </ul>	
+      <Route path="/" component={App} />
+      <Route path="/intel" component={Intel} />
+    </div>
+  </HashRouter>
+)
+
+
+ReactDOM.render(routing, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
